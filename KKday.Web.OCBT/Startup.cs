2dd5080ee1c3df1428596c118e31c164bce3f733
philwang-1990 <phil.wang@kkday.com>
2022-05-09 15:42:39 +0800

    ASP.NET Core多語系挖字

--- KKday.Web.OCBT/Startup.cs
+++ KKday.Web.OCBT/Startup.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace KKday.Web.OCBT
 {
@@ -23,7 +28,34 @@ namespace KKday.Web.OCBT
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            #region ASP.NET Core多語系挖字
+
+            //JSON多語系挖字
+            services.AddJsonLocalization(options => options.ResourcesPath = "Resources");
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("zh-TW"),
+                    new CultureInfo("en-US")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders = new List<IRequestCultureProvider>
+                {
+                    // 依序進行判斷文化特性，以上都沒有才會以 DefaultRequestCulture 決定語系。
+                    new CookieRequestCultureProvider(), // (1) 透過 cookie 的值判斷要求的文化特性資訊。
+                    new QueryStringRequestCultureProvider(), // (2) 透過查詢字串中的值，決定要求的文化特性資訊。
+                    new AcceptLanguageHeaderRequestCultureProvider(), // (3) 透過 Accept-Language 標頭的值，判斷要求的文化特性資訊。 
+                };
+            });
+
+            #endregion ASP.NET Core多語系挖字
+
+            services.AddControllersWithViews().AddViewLocalization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +71,11 @@ namespace KKday.Web.OCBT
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // 多語系挖字初始設定
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
